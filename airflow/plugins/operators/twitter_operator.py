7556976915317799a9c513fdcafaa52ff712eb2a
luis <luis.r.s14@hotmail.com>
2022-06-17 16:44:27 -0300

    exportando dados para Data Lake pt1

--- airflow/plugins/operators/twitter_operator.py
+++ airflow/plugins/operators/twitter_operator.py
@@ -1,18 +1,28 @@
 
 
+from xml.sax import _create_parser
 from airflow.models import BaseOperator, DAG, TaskInstance
 from airflow.utils.decorators import apply_defaults
 from hooks.twitter_hook import TwitterHook
 from datetime import datetime
+from pathlib import Path
 import json
 
 
 class TwitterOperator(BaseOperator):
 
+    template_fields = [
+        "query",
+        "file_path",
+        "start_time",
+        "end_time"
+    ]
+
     @apply_defaults
     def __init__(
         self,
         query,
+        file_path,
         conn_id = None,
         start_time = None,
         end_time = None,
@@ -20,10 +30,13 @@ class TwitterOperator(BaseOperator):
     ):
         super().__init__(*args, **kwargs)
         self.query = query
+        self.file_path = file_path
         self.conn_id = conn_id #or "Twitter_default" 
         self.start_time = start_time
         self.end_time = end_time
 
+    def create_parent_folder(self):
+        Path(Path(self.file_path).parent).mkdir(parents=True, exist_ok=True)
 
     def execute(self, context):
         #hook = TwitterHook(query=self.query, conn_id=self.conn_id, start_time=self.start_time, end_time=self.end_time) #é a mesma coisa
@@ -34,15 +47,21 @@ class TwitterOperator(BaseOperator):
             start_time=self.start_time,
             end_time=self.end_time
         )
+        
+        self.create_parent_folder()
 
-        for pg in hook.run():
-            print(json.dumps(pg, indent=4, sort_keys=True))
+        with open(self.file_path, "w") as output_file:
+            for pg in hook.run():
+                json.dump(pg, output_file, ensure_ascii=False)
+                output_file.write("\n")
 
 
 if __name__ == "__main__":
     with DAG(dag_id="TwitterTest", start_date=datetime.now()) as dag:
-        to = TwitterOperator(query="AluraOnline", task_id="test_run")
+        to = TwitterOperator(query="AluraOnline", 
+        file_path="AluraOnline_{{ ds_nodash }}.json",
+        task_id="test_run")
 
         ti = TaskInstance(task=to, execution_date=datetime.now())
 
-        to.execute(ti.get_template_context())
+        ti.run()
