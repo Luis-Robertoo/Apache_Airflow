0486586cb888119e80f2c871e270d0949d68e5c6
luis <luis.r.s14@hotmail.com>
2022-06-17 13:39:48 -0300

    criando operadores e conectando ao gancho

---
+++ airflow/plugins/operators/twitter_operator.py
@@ -0,0 +1,48 @@
+
+
+from airflow.models import BaseOperator, DAG, TaskInstance
+from airflow.utils.decorators import apply_defaults
+from hooks.twitter_hook import TwitterHook
+from datetime import datetime
+import json
+
+
+class TwitterOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(
+        self,
+        query,
+        conn_id = None,
+        start_time = None,
+        end_time = None,
+        *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.query = query
+        self.conn_id = conn_id #or "Twitter_default" 
+        self.start_time = start_time
+        self.end_time = end_time
+
+
+    def execute(self, context):
+        #hook = TwitterHook(query=self.query, conn_id=self.conn_id, start_time=self.start_time, end_time=self.end_time) #é a mesma coisa
+
+        hook = TwitterHook(
+            query=self.query,
+            conn_id=self.conn_id,
+            start_time=self.start_time,
+            end_time=self.end_time
+        )
+
+        for pg in hook.run():
+            print(json.dumps(pg, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    with DAG(dag_id="TwitterTest", start_date=datetime.now()) as dag:
+        to = TwitterOperator(query="AluraOnline", task_id="test_run")
+
+        ti = TaskInstance(task=to, execution_date=datetime.now())
+
+        to.execute(ti.get_template_context())
