1a68cf8c7bf7580c56ddca6601e93188009b6afa
luis <luis.r.s14@hotmail.com>
2022-06-19 12:58:06 -0300

    integrando Spark ao AirFlow

--- testes.py
+++ testes.py
@@ -18,3 +18,35 @@ for x in g:
     print(x)
 
 
+
+
+
+entrada = 5
+c = 1
+t1 = 0
+t2 = 1
+
+while c < entrada:
+    t3 = t1 + t2
+    #print(" > ", t3, end=" ")
+    t1 = t2
+    t2 = t3
+    c+=1
+print(t3)
+
+
+print('\n','-='*9)
+def fin(n):
+
+    if(n in [0, 1]):
+        return n
+    return fin(n - 1) + fin(n - 2)
+
+
+print(fin(entrada))
+
+print('=-='*4)
+
+
+
+
